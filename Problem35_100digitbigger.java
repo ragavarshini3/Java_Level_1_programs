6cb895c92d7a39cb30c74220a75d8b304411e174
RAGAVARSHINI ALAGARSAMY <152855194+ragavarshini3@users.noreply.github.com>
2026-06-19 09:41:48 +0530

    Add files via upload

---
+++ Problem35_100digitbigger.java
@@ -0,0 +1,12 @@
+package levelprograms;
+import java.util.*;
+public class Problem35_100digitbigger {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		
+	}
+}
